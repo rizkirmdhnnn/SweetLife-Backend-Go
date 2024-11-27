a383574d0a97d805b084ae572d6eb8baca94f034
rizkirmdhnnn <achmadrizkiramadhan0101@gmail.com>
2024-11-28 00:40:48 +0700

    feat: add dummy dashboard route with user progress data

--- routers/routers.go
+++ routers/routers.go
@@ -12,6 +12,37 @@ func Routers(r *gin.Engine) {
 	healthRouter(prefix)
 	recomendationRouter(prefix)
 
+	// dummy route
+	prefix.GET("/dashboard", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"status": "success",
+			"data": gin.H{
+				"user": gin.H{
+					"name":         "Jokowi",
+					"diabetesType": "TYPE_2",
+				},
+				"dailyProgress": gin.H{
+					"calorie": gin.H{
+						"current":      250,
+						"target":       1500,
+						"percentage":   16.67,
+						"satisfaction": "UNDER",
+					},
+					"glucose": gin.H{
+						"current":      250,
+						"target":       1500,
+						"percentage":   16.67,
+						"satisfaction": "UNDER",
+					},
+				},
+				"status": gin.H{
+					"satisfaction": "UNDER", // UNDER (<50%), GOOD (50-100%), EXCEED (>100%)
+					"message":      "Ayo tingkatkan asupan kalori harianmu!",
+				},
+			},
+		})
+	})
+
 	// health check
 	r.GET("/health", func(c *gin.Context) {
 		c.JSON(200, gin.H{
