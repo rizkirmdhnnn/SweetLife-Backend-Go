57ea043996aba30bdb8d4ca877a43f9cca2f6eed
rizkirmdhnnn <achmadrizkiramadhan0101@gmail.com>
2024-12-06 16:08:07 +0700

    ketinggalan kocak

--- dto/scanFoodDto.go
+++ dto/scanFoodDto.go
@@ -17,10 +17,29 @@ type ScanFoodResponse struct {
 type FoodList struct {
 	Name         string  `json:"name"`
 	Unit         int     `json:"unit"`
-	Weight       float64 `json:"weight"`
 	Calories     float64 `json:"calories"`
 	Protein      float64 `json:"protein"`
 	Sugar        float64 `json:"sugar"`
 	Carbohydrate float64 `json:"carbohydrate"`
 	Fat          float64 `json:"fat"`
+}
+
+type FindFoodClientResp struct {
+	Alert         string                  `json:"alert"`
+	FoodName      string                  `json:"food_name"`
+	NutritionInfo NutritionInfoClientResp `json:"nutrition_info"`
+	Weight        float64                 `json:"weight"`
+}
+
+type NutritionInfoClientResp struct {
+	Calories      float64 `json:"calories"`
+	Carbohydrates float64 `json:"carbohydrates"`
+	Fat           float64 `json:"fat"`
+	Proteins      float64 `json:"proteins"`
+	Sugar         float64 `json:"sugar"`
+}
+
+type FindFoodRequest struct {
+	Name   string  `json:"name"`
+	Weight float64 `json:"weight"`
 }
